d9dbeb203a9c9041e412d91e8dd3c327b91e7dd6
MiKL-B <michaelbecquer@yahoo.fr>
2024-04-11 17:13:13 +0200

    update

---
+++ vue-project/src/components/TodoList.vue
@@ -0,0 +1,12 @@
+<template>
+    <Input @add="addTodo" v-model="newTodo" @handlekey="handleEnterKeyTodo" />
+    <Todo v-for="todo in filteredTodos" :key="todo.index" />
+    <TodoSelected :selectedTodo="selectedTodo" />
+  
+</template>
+
+<script>
+export default {
+    name: "TodoList"
+}
+</script>
