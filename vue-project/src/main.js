3989b9727d02c7ef1b6156048a79f473e0ed697d
MiKL-B <michaelbecquer@yahoo.fr>
2024-04-19 20:24:28 +0200

    pwa

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createI18n } from 'vue-i18n';
 import App from './App.vue';
 import messages from './plugins/i18n.js';
+import './registerServiceWorker'
 
 const app = createApp(App);
 const i18n = createI18n(messages);
